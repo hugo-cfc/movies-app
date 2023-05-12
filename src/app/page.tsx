9df7b97237cd2d8823b69d6b382db7a83a2dc360
Hugo Cesar Firmino Costa <hugocesar@MacBook-Air-de-Hugo.local>
2023-05-12 16:59:28 -0300

    chore: installed tailwind, global css ready and next-themes installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 // import Image from "next/image";
-import styles from "./page.module.css";
 
 export const metadata = {
   title: "adad",
 };
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>Hello, world!</h1>
-    </main>
-  );
+  return <h1 className="text-3xl font-bold underline ">Hello world!</h1>;
 }
--- src/services/fetchWrapper.ts
+++ src/services/fetchWrapper.ts
@@ -3,7 +3,7 @@ export async function fetchWrapper<T = unknown>(
   init?: RequestInit | undefined
 ) {
   const data = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/${input}`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/${input}?api_key=161a90d5311a80fc15c2a62eb7a63f64`,
     init
   );
   const result = await data.json();
