5f135b3f67a499f4a471b7e05e6d81c3270618a5
Hugo Cesar Firmino Costa <hugocesar@MacBook-Air-de-Hugo.local>
2023-05-14 16:41:21 -0300

    feat: set PT-BR language, movie type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+// import { fetchWrapper } from "@/services/fetchWrapper";
+
 import DarkModeButton from "../components/isolated/DarkModeButton";
 
 export const metadata = {
-  title: "adad",
+  title: "Movies App",
 };
 
 export default function Home() {
+  // const res = await fetchWrapper("trending/all/day");
+
   return (
     <>
       <DarkModeButton />
