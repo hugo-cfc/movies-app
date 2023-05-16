016200f8df514641f4a61713e6b0120a53faa800
Hugo Cesar Firmino Costa <hugocesar@MacBook-Air-de-Hugo.local>
2023-05-15 23:52:23 -0300

    feat: header is ready

---
+++ src/components/isolated/Header/strings.ts
@@ -0,0 +1,17 @@
+export const menuItems: { id: string; title: string; path: string }[] = [
+  {
+    id: crypto.randomUUID(),
+    title: "Filmes",
+    path: "/",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Séries",
+    path: "/series",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "TV",
+    path: "/tv-shows",
+  },
+];
