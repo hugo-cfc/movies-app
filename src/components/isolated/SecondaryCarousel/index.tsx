f9a0cb966b434fc80bba79863b95d2092538e8a9
Hugo Cesar Firmino Costa <hugocesar@MacBook-Air-de-Hugo.local>
2023-06-10 00:41:22 -0300

    feat: tablet responsiveness ready

--- src/components/isolated/SecondaryCarousel/index.tsx
+++ src/components/isolated/SecondaryCarousel/index.tsx
@@ -32,19 +32,21 @@ const SecondaryCarousel = ({
   });
 
   return (
-    <div className="w-[300px] m-auto h-fit mt-8 tablet:mt-[4.63rem] desktop:w-[1280px]">
+    <div className="w-[300px] m-auto h-fit mt-8 tablet:mt-12 tablet:w-[620px] desktop:w-[1280px]">
       <div className="grid grid-cols-4 tablet:grid-cols-8 notebook:grid-cols-12 notebook:gap-x-4 mb-7">
-        <div className="flex items-center justify-start col-start-1 col-end-4 notebook:col-end-6">
+        <div className="flex items-center justify-start col-start-1 col-end-4 tablet:col-end-6 notebook:col-end-6">
           <Image
-            className="w-6 mr-1.5 tablet:w-auto notebook:mr-[1.13rem]"
+            className="w-6 mr-1.5 tablet:w-auto tablet:mr-[1.13rem]"
             src={sectionIcon}
             alt={title}
           />
-          <h1 className="text-white text-sm desktop:text-[2rem]">{title}</h1>
+          <h1 className="text-white text-sm tablet:text-2xl desktop:text-[2rem]">
+            {title}
+          </h1>
         </div>
 
         <Link
-          className="text-white text-sm flex items-center col-start-4 col-end-5 tablet:text-2xl notebook:col-start-11 notebook:col-end-13"
+          className="text-white text-sm flex items-center col-start-4 col-end-5 tablet:col-start-7 tablet:col-end-9 tablet:text-2xl notebook:col-start-11 notebook:col-end-13"
           href={seeMoreUrl}
         >
           Ver Mais
@@ -55,7 +57,7 @@ const SecondaryCarousel = ({
           />
         </Link>
 
-        <div className="mt-2 bg-gradient-to-t from-greenPrimary to-bluePrimary h-[2px] col-start-1 col-end-13" />
+        <div className="mt-2 bg-gradient-to-t from-greenPrimary to-bluePrimary h-[2px] col-start-1 col-end-13 tablet:mt-3" />
       </div>
 
       <div className="flex items-center justify-center">
